53d3f9fbaa70d823512d10ed03f104b614ab83d7
Luis Levano <n00174830@upn.pe>
2026-09-06 11:38:33 -0500

    feat: implementa codigo en la clase Menu.java

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,10 +4,41 @@
  */
 package menu;
 
-/**
- *
- * @author USER
- */
+import java.util.Scanner;
+
 public class Menu {
-    
+    public static void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("================================");
+            System.out.println("        LIBRERÍA NOVA           ");
+            System.out.println("================================");
+            System.out.println("1. Consultar productos");
+            System.out.println("2. Registrar cliente");
+            System.out.println("3. Registrar pedido");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("\n[Consultando productos...]\n");
+                    break;
+                case 2:
+                    System.out.println("\n[Registrando cliente...]\n");
+                    break;
+                case 3:
+                    System.out.println("\n[Registrando pedido...]\n");
+                    break;
+                case 4:
+                    System.out.println("\nSaliendo del sistema...\n");
+                    break;
+                default:
+                    System.out.println("\nOpción inválida. Intente de nuevo.\n");
+            }
+        } while (opcion != 4);
+    }
 }

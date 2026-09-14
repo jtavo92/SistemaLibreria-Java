10906262dc0a30bb798e1e02467ac1e8c8889e8b
kevin osco <kevinosco0@gmail.com>
2026-09-13 22:01:01 -0500

    Completa menu y gestion basica de libreria

--- SistemaLibreria-Java/src/main/java/Main.java
+++ SistemaLibreria-Java/src/main/java/Main.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import menu.Menu;
 
-/**
- *
- * @author ADRIANO
- */
 public class Main {
 
     public static void main(String[] args) {
-
-        Producto producto = 
-                new Producto("Cuaderno Nova A4", 12.50);
-
-        producto.mostrarDatos();
+        Menu.mostrarMenu();
     }
 }
